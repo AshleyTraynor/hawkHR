f77bf33aac8719a38ec369137b0dfbbcc3362d30
Ashley Traynor <amoore357@gmail.com>
2016-07-18 18:08:27 +0100

    SQL Test page, not to be used

--- App_Code/HAWK.SQL.vb
+++ App_Code/HAWK.SQL.vb
@@ -22,19 +22,36 @@ Namespace HAWK
             End If
 
             Dim cmd As New SqlCommand
-            Dim rdr As SqlDataReader
 
             With cmd
                 .Connection = conn
-                .CommandText =
-                .CommandType =
+                .CommandText = "[Birdy].[getAccountTypeName]"
+                .CommandType = CommandType.StoredProcedure
                 .Parameters.Add("@AccountTypeID", Data.SqlDbType.TinyInt).Value = AccountTypeID
+                .Parameters.Add("@AccountTypeName", Data.SqlDbType.VarChar, 30).Direction = ParameterDirection.Output
             End With
 
-            If conn.State = ConnectionState.Closed Then
+            Try
+                If conn.State = ConnectionState.Closed Then
+                    System.Diagnostics.Debug.WriteLine("open conn")
+                    conn.Open()
+                End If
 
-            End If
+                System.Diagnostics.Debug.WriteLine("Executing getAccountTypeName Stored Procedure")
+                cmd.ExecuteNonQuery()
+                AccountTypeName = cmd.Parameters("@AccountTypeName").Value
+            Catch ex As Exception
+                Diagnostics.Debug.WriteLine(ex.Message)
+            Finally
+                cmd.Dispose()
+                If closeConn Then
+                    System.Diagnostics.Debug.WriteLine("close conn")
+                    conn.Close()
+                    conn.Dispose()
+                End If
+            End Try
 
+            Return AccountTypeName
 
         End Function
 
