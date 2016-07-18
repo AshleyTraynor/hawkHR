eba25fb3ce9c2315d0e324c07882c716c60c1649
Ashley Traynor <amoore357@gmail.com>
2016-07-18 01:03:48 +0100

    Added HAWK.SQL.vb -- Still work to do on that

---
+++ App_Code/HAWK.SQL.vb
@@ -0,0 +1,43 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Data.SqlClient
+Imports System.Data
+Imports System.IO
+Imports System.Runtime.Serialization.Formatters.Binary
+Imports System.IO.Compression
+
+Namespace HAWK
+
+    Public Module SQL
+
+        Public Function getHawkConnection() As SqlConnection
+            Return New SqlConnection(ConfigurationManager.ConnectionStrings("hawkHRConnection").ConnectionString)
+        End Function
+
+        Public Function getAccountTypeName(ByVal AccountTypeID As Integer, Optional ByRef conn As SqlConnection = Nothing, Optional closeConn As Boolean = True) As String
+
+            Dim AccountTypeName As String
+
+            If conn Is Nothing Then
+                conn = getHawkConnection()
+            End If
+
+            Dim cmd As New SqlCommand
+            Dim rdr As SqlDataReader
+
+            With cmd
+                .Connection = conn
+                .CommandText =
+                .CommandType =
+                .Parameters.Add("@AccountTypeID", Data.SqlDbType.TinyInt).Value = AccountTypeID
+            End With
+
+            If conn.State = ConnectionState.Closed Then
+
+            End If
+
+
+        End Function
+
+    End Module
+
+End Namespace
